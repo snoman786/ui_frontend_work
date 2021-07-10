cec0f0c984a23cf976d6d23971b192843db75c33
snoman786 <syedmohd.noman@gmail.com>
2021-07-10 19:16:42 +0530

    Navigation setup

---
+++ src/components/product/list/ProductList.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+function ProductList(props) {
+
+    return(
+        <div>
+            <h1>Product List</h1>
+        </div>
+    );
+
+}
+
+export default ProductList;
--- src/components/router/RouterComponent.jsx
+++ src/components/router/RouterComponent.jsx
@@ -4,18 +4,20 @@ import AddUserComponent from '../user/add/AddUserComponent';
 import EditUserComponent from '../user/edit/EditUserComponent';
 import React from "react";
 import './ReactRouter.css';
+import ProductList from '../product/list/ProductList';
 
 const AppRouter = () => {
     return(
         <div>
             <Router>
                 <div className="container">
-                    <h1 style={style}>React User Application</h1>
+                    <h1 style={style}>React UI Application</h1>
                     <Switch>
                         <Route path="/" exact component={ListUserComponent} />
                         <Route path="/users" component={ListUserComponent} />
                         <Route path="/add-user" component={AddUserComponent} />
                         <Route path="/edit-user" component={EditUserComponent} />
+                        <Route path= "/products" component ={ProductList} />
                     </Switch>
                 </div>
             </Router>
